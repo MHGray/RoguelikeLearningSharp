61a3a7cf13f3359bf7c6af6b8074a17c6be6cc68
phlip45 <phlip45@Gmail.com>
2019-03-15 22:48:40 -0700

    Unfuck this please?

--- SimpleGame/Map/Map.cs
+++ SimpleGame/Map/Map.cs
@@ -19,6 +19,7 @@ namespace SimpleGame
             Height = height;
             for (int j = 0; j < height; j++)
             {
+
                 for (int i = 0; i < width; i++)
                 {
                     _tiles.Add(new Tile(i, j));
